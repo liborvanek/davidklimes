3dbebf6e220ce1c59f10ba99aa4e3d6a5d6bb6b9
Martin Franek <martin.ffranek@gmail.com>
2021-01-16 20:08:35 +0000

    add basic showcase of rss feed for komentare section

--- src/server.js
+++ src/server.js
@@ -1,17 +1,38 @@
+import { config as dotenvConfig } from 'dotenv-safe';
 import sirv from 'sirv';
-import Express from 'express';
+import express from 'express';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+import mongoose from 'mongoose';
+
+dotenvConfig();
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-Express()
+mongoose.connect(process.env.MONGO_URL, {
+	useUnifiedTopology: true,
+	useNewUrlParser: true,
+});
+
+export const db = mongoose.connection;
+
+db.once('open', () => {
+	console.log('Connected to mongodb.');
+}).on('error: ', (error) => {
+	throw new Error(`Disconnected from mongodb. Reason: ${error}`);
+});
+
+const server = express();
+
+server
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware(),
 	)
-	.listen(PORT, (err) => {
-		if (err) console.log('error', err);
+	.listen(PORT, (error) => {
+		if (error) {
+			console.error('error', error);
+		}
 	});
---
+++ src/utils.js
@@ -0,0 +1,5 @@
+export function formatDate(dateString) {
+	const date = new Date(dateString);
+
+	return `${date.getDate()}. ${date.getMonth() + 1}. ${date.getFullYear()}`;
+}
