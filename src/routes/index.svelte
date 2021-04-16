8abd82abf210bd8103dc886ea6a0bbf6a4bd7310
Libor Vaněk <lv@liborvanek.cz>
2021-04-16 09:00:10 +0200

    Update Clubhouse invitation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -18,16 +18,17 @@
 
   const mondayClubhouse = {
     subtitle: 'Pondělní clubhouse',
-    title: 'O rozvolňování i ODS s hejtmanem Martinem Kubou',
-    link: 'https://www.joinclubhouse.com/event/xXzzZJq4',
-    isoDate: '2021-04-12T20:30:00.000Z',
-    date: `<div class="flex items-center"><img class="w-6 mr-2 inline-block" src="icon/clubhouse-wave.png" alt="Clubhouse icon"/><span>12. 4. 2021, 20:00</span></div>`,
+    title: 'Kam směřuje česká zahraniční politika?',
+    annotation: 'Special Guest: šéfredaktorka českého Euractivu Aneta Zachová.',
+    link: 'https://www.joinclubhouse.com/event/mJ6knwjV',
+    isoDate: '2021-04-19T20:00:00.000Z',
+    date: `<div class="flex items-center"><img class="w-6 mr-2 inline-block" src="icon/clubhouse-wave.png" alt="Clubhouse icon"/><span>19. 4. 2021, 20:00</span></div>`,
   };
   onMount(() => {
     // This promise will replace the first one and will be resolved in the {#await} block
     // It will run on client only
     latestArticlePromise = promiseMinDelay(
-      new Date().getDay() < 2
+      new Date().getDay() > 2
         ? Promise.resolve(mondayClubhouse)
         : fetch('/api/latest-article').then((body) => body.json()),
       1000,
@@ -45,7 +46,7 @@
 {#if $showNewsletterIntro}
   <NewsletterOutro />
 {:else}
-  {#await latestArticlePromise then { title, subtitle, link, date }}
+  {#await latestArticlePromise then { title, subtitle, annotation, link, date }}
     <aside
       in:clipRect={{ duration: 500, delay: 0 }}
       class="hidden lg:block absolute px-8 py-4 lg:pt-10 lg:pl-12 lg:pr-12 lg:pb-12 lg:w-96 lg:top-16 right-8 xl:right-16 2xl:right-24 transform -rotate-1 bg-gray-50 dark:bg-gray-800"
@@ -57,6 +58,9 @@
           class="dotted inline-block font-bold text-gray-600 visited:text-gray-600 dark:text-gray-300 dark:visited:text-gray-300 hover:text-blue-800 dark:hover:text-blue-500 underline transition-colors"
           >{@html bindSingles(title)}</Link>
       </h2>
+      {#if annotation}
+        <p class="mt-4 text-sm text-gray-500">{annotation}</p>
+      {/if}
       {#if date}
         <p class="mt-4 text-xs text-gray-400">{@html date}</p>
       {/if}
