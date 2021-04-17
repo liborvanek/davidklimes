6679c03ee8bca1593f85d0ec361d9ff09962000b
Libor Vaněk <lv@liborvanek.cz>
2021-04-17 13:15:03 +0200

    Remove reload button from error page

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -18,22 +18,5 @@
         {status === 404 ? 'Stránka nebyla nalezena' : 'Bohužel, stránku se nepodařilo načíst'}
       </h2>
     {/if}
-    {#if status !== 404}
-      <button
-        class="inline-flex mt-8 p-2 items-center border-1 border-blue-600 rounded-md text-xs text-blue-600 uppercase"
-        on:click={() => location.reload()}
-        ><svg
-          class="w-4 h-4 mr-2"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-          ><path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
-        </svg>Obnovit stránku</button>
-    {/if}
   </section>
 </PageTransition>
