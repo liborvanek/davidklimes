56936ca988bf8a1d37eaeacceb21c2033f8fe025
Libor Vaněk <lv@liborvanek.cz>
2021-09-14 21:52:48 +0200

    Update author colors

--- src/components/PovolebniProgram/Comment.svelte
+++ src/components/PovolebniProgram/Comment.svelte
@@ -1,8 +1,8 @@
 <blockquote class="pb-2">
   <slot />
   <footer>
-    <cite class="text-md not-italic text-gray-600"
-      ><span class="font-bold"><slot name="author" /></span> &mdash; <slot
-        name="occupation" /></cite>
+    <cite class="text-md not-italic text-gray-600 dark:text-gray-400"
+      ><span class="font-bold text-gray-700 dark:text-gray-300"><slot name="author" /></span>
+      &mdash; <slot name="occupation" /></cite>
   </footer>
 </blockquote>
